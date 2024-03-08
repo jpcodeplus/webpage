dc95a4fd8f0a955afd83ff9abeb75dd4933d3d24
jpcodeplus <behrens.codeplus@gmail.com>
2024-03-08 12:50:01 +0100

    added: primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,23 @@ export default {
   purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': {
+          '50': '#f3f7fc',
+          '100': '#e6eff8',
+          '200': '#c7dcf0',
+          '300': '#96bfe3',
+          '400': '#5e9ed2',
+          '500': '#3b86c3',
+          '600': '#2968a0',
+          '700': '#225382',
+          '800': '#20476c',
+          '900': '#1f3d5b',
+          '950': '#15273c'
+      }
+      },
+    },
   },
   variants: {
     extend: {},
